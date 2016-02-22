bd22f5b99ffd3253f0d7b35596f281543f30233f
1forintos <xrensis@gmail.com>
2016-02-22 01:27:11 +0100

    Added monitoring of face found via digital GPIO pin 5.

--- raspberry/camstab.py
+++ raspberry/camstab.py
@@ -12,19 +12,6 @@ import RPi.GPIO as GPIO
 import wiringpi2 as wiringpi
 import time
 
-PWM_PIN_1 = 16
-PWM_PIN_2 = 21
-
-# GPIO.setwarnings(False)
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(PWM_PIN_1, GPIO.OUT)
-# GPIO.setup(PWM_PIN_2, GPIO.OUT)
-
-# pwm1 = GPIO.PWM(PWM_PIN_1, 320)
-# pwm2 = GPIO.PWM(PWM_PIN_2, 320)
-# pwm1.start(1)
-# pwm2.start(1)
- 
 # use BCM GPIO numbers                                                        
 wiringpi.wiringPiSetupGpio()
 
@@ -32,8 +19,6 @@ pwm_pin_horizontal = 18
 pwm_pin_vertical = 13
 
 pwm_clock = 200
-
-
 pwm_range = 1024
 
 dt_min = 125
@@ -44,23 +29,43 @@ dt_vertical = 150
 
 # enable PWM0                                                                 
 wiringpi.pinMode(pwm_pin_horizontal, 2)
-wiringpi.pwmSetMode(0)
-wiringpi.pwmSetClock(pwm_clock)
-wiringpi.pwmSetRange(pwm_range)
 
 # enable PWM1                                                                 
 wiringpi.pinMode(pwm_pin_vertical, 2)
-wiringpi.pwmSetMode(0)
+
+#led
+faceFoundPin = 5
+wiringpi.pinMode(faceFoundPin, 1)
+
 wiringpi.pwmSetClock(pwm_clock)
 wiringpi.pwmSetRange(pwm_range)
 
+#wiringpi.pwmSetClock(pwm_clock)
+#wiringpi.pwmSetRange(pwm_range)
+
 time_start = time.time()
-cycle = 5		
 
 wiringpi.pwmWrite(pwm_pin_horizontal, dt_horizontal)
 wiringpi.pwmWrite(pwm_pin_vertical, dt_vertical)
 
-time.sleep(1)
+#pwmSignal = 0
+#ledOn = False
+#while True:
+#	if time.time() - time_start > 1:
+#        	if ledOn == True:
+#                	ledOn = False
+#                else:
+#                	ledOn = True
+#                wiringpi.digitalWrite(5, ledOn)
+#                wiringpi.digitalWrite(pwm_pin_vertical, ledOn)
+#                time_start = time.time()
+#	pwmSignal = pwmSignal + 0.02
+#        if pwmSignal > 300:
+#        	pwmSignal = 0
+#	print("PWM YO", pwmSignal)		
+#       wiringpi.pwmWrite(pwm_pin_horizontal, int(pwmSignal))
+
+time.sleep(0.2)
 
 def transmitMoveOrder(horizontal, vertical):
 	global dt_horizontal
@@ -79,8 +84,8 @@ def transmitMoveOrder(horizontal, vertical):
 		dt_vertical = dt_min
 	wiringpi.pwmWrite(pwm_pin_vertical, int(dt_vertical))
 
-	print("Horizontal: ",dt_horizontal)
-	print("Vertical: ",dt_vertical)
+	# print("Horizontal: ",dt_horizontal)
+	# print("Vertical: ",dt_vertical)
 	time.sleep(0.01)
 	return 1
  
@@ -96,10 +101,13 @@ def checkPosition(posX, posY, maxX, maxY):
 	maxDistanceY = maxY / 2
 
 	# required distance from center to object (in percent of screen) to send move order
-	threshold = 8 # percent
-	maxSpeed = 600
-	minSpeed = 100
-	speedRange = maxSpeed - minSpeed
+	thresholdX = 20 # percent
+	thresholdY = 16
+
+#	maxSpeed = 600
+#	minSpeed = 100
+
+#	speedRange = maxSpeed - minSpeed
 
 	# if distance does not hit threshold leave it to zero
 	moveX = 0.0
@@ -108,20 +116,39 @@ def checkPosition(posX, posY, maxX, maxY):
 	relativePosX = posX - centerX # negative if left
 	relativePosY = posY - centerY # negative if up
 	
-	maxMove = 4.0
-	if distanceX > maxX * (0.01 * threshold):
+	maxMoveX = 8.0
+	maxMoveY = 8.0
+
+	if distanceX > maxX * (0.01 * thresholdX):
 		if relativePosX < 0:	# move left
-			moveX = -1 * maxMove # (distanceX / maxDistanceX) * maxMove
-			# print("LEFT")
+			# relative speed
+			#moveX = -1 * (distanceX / maxDistanceX) * maxMoveX
+
+			# static speed
+			moveX = -1 * maxMoveX
+
+			#print("LEFT")
 		else:
-			moveX =  maxMove #(distanceX / maxDistanceX) * maxMove
+			# relative speed 
+			#moveX =  maxMoveX * (distanceX / maxDistanceX) * maxMoveX
+
+			# static speed
+			moveX = maxMoveX
+
 			# print("RIGHT")
-	if distanceY > maxY * (0.01 * threshold):
+	if distanceY > maxY * (0.01 * thresholdY):
 		if relativePosY < 0:	# move up
-			# print("UP")
-			moveY = -1 * maxMove #(distanceY / maxDistanceY) * maxMove
-		else:				# move down
-			moveY =  maxMove #(distanceY / maxDistanceY) * maxMove					
+			# relative speed 
+			#moveY = -1 * (distanceY / maxDistanceY) * maxMoveY
+			
+			# static speed
+			moveY = -1 * maxMoveY
+		else:			# move down
+			# relative speed 
+			#moveY = (distanceY / maxDistanceY) * maxMoveY					
+			
+			# static speed
+			moveY = maxMoveY
 			# print("DOWN")
 	transmitMoveOrder(moveX, moveY);
 
@@ -136,62 +163,81 @@ ap.add_argument("-d", "--display", type=int, default=-1,
 args = vars(ap.parse_args())
 
 # created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
 print("[INFO] sampling THREADED frames from webcam...")
 vs = WebcamVideoStream(src=0).start()
-# fps = FPS().start()
 
 #Load a cascade file for detecting faces
 face_cascade = cv2.CascadeClassifier('facecascade.xml')
-#start_time = time.time()
+start_time = time.time()
 
 frame = vs.read()
 imageHeight, imageWidth = frame.shape[:2]
 
+fpsCounter = 0
 # loop over some frames...this time using the threaded stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
 
- 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #Look for faces in the image using the loaded cascade file
-	faces = face_cascade.detectMultiScale(gray, 1.8, 5)
+try:
+	while True:
+		currTime = time.time()
+		if currTime - start_time >= 1:
+			print("FPS: ", fpsCounter)
+			fpsCounter = 0
+			start_time = currTime
+		# grab the frame from the threaded video stream and resize it
+		# to have a maximum width of 400 pixels
+		frame = vs.read()
 
-	print ("Found ", len(faces))
+	 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	        #Look for faces in the image using the loaded cascade file
+		faces = face_cascade.detectMultiScale(gray, 1.8, 3)
 	
-	firstFace = True
-	#Draw a rectangle around every found face
-	for (x, y, w, h) in faces:
-		if firstFace:
-			cv2.rectangle(frame,(x, y), (x + w, y + h), (0, 255, 0), 2)
-			checkPosition(x + w / 2, y + h / 2, imageWidth, imageHeight)
-			firstFace = False
-		else: 
-			cv2.rectangle(frame,(x, y), (x + w, y + h), (0, 0, 255), 2)				
+		print ("Found ", len(faces))
 		
-	if len(faces) == 0:
-		transmitMoveOrder(0, 0)
-	# check to see if the frame should be displayed to our screen
-	if args["display"] > 0:
-		#elapsed_time = time.time() - start_time
+		if len(faces) == 0:
+                        #print("DIGITAL WRITE 0")
+                        wiringpi.digitalWrite(faceFoundPin, 0)
+                        transmitMoveOrder(0, 0)
+                else:
+                        #print("DIGITAL WRITE 1")
+                        wiringpi.digitalWrite(faceFoundPin, 1)
 
-		frame = imutils.resize(frame, width=400)
-		# if elapsed_time > 2:
-		#start_time = time.time()		
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-
- 	# cv2.imwrite('result.jpg', frame)
-	# update the FPS counter
-	# fps.update()
- 	# print("--- frame processed ---")
-
-# stop the timer and display FPS information
-# fps.stop()
-#print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-#print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
- 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+		firstFace = True
+		#Draw a rectangle around every found face
+		for (x, y, w, h) in faces:
+			if firstFace:
+				cv2.rectangle(frame,(x, y), (x + w, y + h), (0, 255, 0), 2)
+				checkPosition(x + w / 2, y + h / 2, imageWidth, imageHeight)
+				firstFace = False
+			else: 
+				cv2.rectangle(frame,(x, y), (x + w, y + h), (0, 0, 255), 2)				
+			
+		# check to see if the frame should be displayed to our screen
+		if args["display"] > 0:
+			#elapsed_time = time.time() - start_time
+			
+			frame = imutils.resize(frame, width=400)
+			# if elapsed_time > 2:
+			#start_time = time.time()		
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+	
+		fpsCounter = fpsCounter + 1
+	 	# print("--- frame processed ---")
+	 
+except KeyboardInterrupt:  
+	# here you put any code you want to run before the program   
+	# exits when you press CTRL+C  
+	print("Keyboard interrupt")
+except:  
+	# this catches ALL other exceptions including errors.  
+	# You won't get any error messages for debugging  
+	# so only use it once your code is working  
+	print("Other error or exception occurred!")
+finally:  
+	print("Cleaning up...")
+	wiringpi.pinMode(pwm_pin_vertical, 0)
+	wiringpi.pinMode(pwm_pin_horizontal, 0)
+	wiringpi.pinMode(faceFoundPin, 0)
+	# GPIO.cleanup() # this ensures a clean exit  
+	cv2.destroyAllWindows()
+        vs.stop()
